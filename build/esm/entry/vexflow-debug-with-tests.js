8a6712d21dc410ee2e279b5350e8897c82bcd175
Rodrigo Vilar <rvilarl@gmail.com>
2023-10-21 10:52:28 +0200

    Remove build/ after releasing version 5.0.0-alpha.9 to npm and GitHub.

--- build/esm/entry/vexflow-debug-with-tests.js
+++
@@ -1,7 +0,0 @@
-import * as VexSrc from '../src/index.js';
-import * as VexTests from '../tests/index.js';
-import { Flow } from '../src/flow.js';
-Flow.setFonts('Bravura', 'Roboto Slab');
-export * from '../src/index.js';
-export * from '../tests/index.js';
-export default Object.assign(Object.assign({}, VexSrc), VexTests);
--- build/esm/entry/vexflow-debug.js
+++
@@ -1,4 +0,0 @@
-import { Flow } from '../src/flow.js';
-Flow.setFonts('Bravura', 'Roboto Slab');
-export * from '../src/index.js';
-export * as default from '../src/index.js';
