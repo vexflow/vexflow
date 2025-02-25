0ca6f889545c33cce851b420c24945f6eb685aeb
Rodrigo Vilar <rvilarl@gmail.com>
2025-02-25 14:09:34 +0100

    Remove build/ after releasing version 5.0.0-beta.1 to npm and GitHub.

--- build/esm/entry/vexflow-core.js
+++
@@ -1,4 +0,0 @@
-import { VexFlow } from '../src/vexflow.js';
-VexFlow.BUILD.INFO = 'vexflow-core';
-export * from '../src/index.js';
-export default VexFlow;
--- build/types/src/stavesection.d.ts
+++
@@ -1,8 +0,0 @@
-import { StaveModifier } from './stavemodifier';
-export declare class StaveSection extends StaveModifier {
-    static get CATEGORY(): string;
-    protected drawRect: boolean;
-    constructor(section: string, x?: number, yShift?: number, drawRect?: boolean);
-    setDrawRect(drawRect: boolean): this;
-    draw(): void;
-}
